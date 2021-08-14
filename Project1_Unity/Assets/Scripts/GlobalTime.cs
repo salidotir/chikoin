eb423d9b5f69efd6dde865d7bcf551b99bcfcb8a
Sara Limooee <salidotir@gmail.com>
2021-08-14 13:36:50 +0430

    add timer to game

---
+++ Project1_Unity/Assets/Scripts/GlobalTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalTime : MonoBehaviour {
+
+	public GameObject timeDisplay;
+	public int seconds = 30;
+	public bool is_time_finished;
+
+	// Update is called once per frame
+	void Update () {
+		if (is_time_finished == false)
+        {
+			is_time_finished = true;
+			StartCoroutine(reduceSeconds());
+        }
+	}
+
+	IEnumerator reduceSeconds()
+    {
+		yield return new WaitForSeconds(1);
+		seconds -= 1;
+		timeDisplay.GetComponent<Text>().text = "Time: " + seconds;
+		is_time_finished = false;
+    }
+}
