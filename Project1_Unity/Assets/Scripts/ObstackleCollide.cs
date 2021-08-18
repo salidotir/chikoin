305e1aeef8b7472412471187ca5c40d164f8ac8f
Sara Limooee <salidotir@gmail.com>
2021-08-18 18:21:12 +0430

    add obstacles to the game

---
+++ Project1_Unity/Assets/Scripts/ObstackleCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ObstackleCollide : MonoBehaviour {
+
+    public GameObject HitObstackleText;
+    public GameObject FadeOut;
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player.GetComponent<PlayerControls>().enabled = false;
+            HitObstackleText.SetActive(true);
+            FadeOut.SetActive(true);
+
+            StartCoroutine(restartLevel());
+        }
+    }
+
+    IEnumerator restartLevel()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+}
