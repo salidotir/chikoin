6199c59acf0d7bcecc880a2b544ef6299a369cdd
Sara Limooee <salidotir@gmail.com>
2021-08-19 15:22:02 +0430

    add basic pause menu

---
+++ Project1_Unity/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public bool gamePaused = false;
+	public GameObject pauseMenu;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel"))
+        {
+			if (gamePaused == false)
+            {
+				Time.timeScale = 0;         // stop time
+				gamePaused = true;
+				Cursor.visible = true;							// make the mouse cursor visible
+				this.GetComponent<AudioSource>().Pause();		// stop music
+				pauseMenu.SetActive(true);
+            }
+
+			else
+            {
+				pauseMenu.SetActive(true);
+				this.GetComponent<AudioSource>().UnPause();
+				Cursor.visible = false;
+				gamePaused = false;
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
